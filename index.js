25a6985d30c965424183c8232e002694f79c411b
行比拓也 <imac@iMac.local>
2020-07-10 22:16:44 +0900

    tscの実行

---
+++ index.js
@@ -0,0 +1,17 @@
+var lines = [];
+var reader = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+reader.on('line', function (line) {
+    lines.push(line);
+});
+reader.on('close', function () {
+    var num1 = 1;
+    var num2 = lines[0];
+    console.log(num1 * num2);
+});
+// 実行方法
+// ./node_modules/.bin/ts-node ファイル名
+// 例1
+// ./node_modules/.bin/ts-node index.ts
